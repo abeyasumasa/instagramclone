6fa4ed1a4a4c6fe6b3adf92a3c0fe1f02989c4c8
yasumasa abe <abeyasumasa@MacBook-Air.local>
2019-07-18 00:14:12 +0900

    Add:CRUD機能を追加 instagram機能に必要なため

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,57 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :edit, :update]
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    if params[:back]
+      @photo = Photo.new(photo_params)
+    else
+      @photo = Photo.new
+    end
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_path, notice: "ブログを作成しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @photo.update(photo_params)
+      redirect_to photos_path, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def  destroy
+    @photo.destroy
+    redirect_to photos_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @photo = Photo.new(photo_params)
+    render :new if @photo.invalid?
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:content)
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 end
