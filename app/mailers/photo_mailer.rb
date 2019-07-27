c6857b34bed61826e48ea79b5f38c746eff775dd
yasumasa abe <abeyasumasa@MacBook-Air.local>
2019-07-28 01:45:20 +0900

    Fix:ActionMailerを修正 要件のため

--- app/mailers/photo_mailer.rb
+++ app/mailers/photo_mailer.rb
@@ -1,7 +1,6 @@
 class PhotoMailer < ApplicationMailer
-  def photo_mail(user)
+  def photo_mail(photo)
     @photo = photo
-    binding.pry
 
     mail to: "abeyasumasa@gmail.com", subject: "写真の投稿確認メール"
   end
